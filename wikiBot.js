e856d2b0b2605acff9da22b8df234f050b2f26e6
Artemiy <tema.istushkin@gmail.com>
2019-11-26 14:24:48 +0100

    Update

--- wikiBot.js
+++ wikiBot.js
@@ -5,14 +5,29 @@ const { ActivityHandler } = require('botbuilder');
 
 var yesArray = ["t", "T", "tak", "Tak", "y", "Y", "yes", "Yes"]
 var noArray = ["n", "N", "nie", "Nie", "no", "No"]
+var dataBase = ["cosinus", "sinus", "tangens", "contangens"]
+
+function findKeywords(input) {
+    var key = input.replace(/[\?\-\+\'\"\`\:\;\[\]\(\)\#\!\.\,\&\@]/g, "");
+    var keys = key.toLowerCase().split(" ");
+    var keyword = []
+    for (var i = 0; i < keys.length; i++) {
+        for (var a = 0; a < dataBase.length; a++) {
+            if (keys[i] == dataBase[a]) {
+                keyword.push(keys[i]);
+            }
+        }
+    }
+    return keyword.join(" ")
+}
 
 class WikiBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            console.log(context.activity.text)
-            var searchQuery = context.activity.text
+            var searchQuery = findKeywords(context.activity.text);
+            console.log(searchQuery)
             searchQuery.trim();
             const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
             await fetch(endpoint)
@@ -21,7 +36,7 @@ class WikiBot extends ActivityHandler {
                     const results = data.query.search;
                     results.forEach(result => {
                         const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`);
-                        console.log(context.sendActivity(`${result.title}`))
+                        context.sendActivity(`${result.title} ${url}`);
                     });
                 })
                 .catch(() => console.log('An error occurred'));
